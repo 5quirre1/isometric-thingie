9326a18af9583fa7a779c4a8c26893adbb21fb29
Squirrel <squirrelhomebrew@gmail.com>
2025-10-31 00:12:01 -0700

    ddd

--- src/game.cpp
+++ src/game.cpp
@@ -3,6 +3,7 @@
 #include <raylib.h>
 #include <hexrgb.h>
 #include <string>
+
 extern std::string g_swagPath; // get the path for the map
 
 // game
@@ -12,13 +13,15 @@ void run_game()
     world.LoadFromSwag(g_swagPath);
 
     const int screenWidth = 800;
+    const int screenHeight = 600;
     const float yOffset = 100.0f;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(fixColors("#c4ccd1ff"));
-        world.DrawWorld(screenWidth, yOffset);
+            ClearBackground(fixColors("#c4ccd1ff"));
+            world.objectManager.DrawSkybox(screenWidth, screenHeight);
+            world.DrawWorld(screenWidth, yOffset);
         EndDrawing();
     }
 
